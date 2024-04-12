4e2e0f831401d36483893236d1bab9851ac111a6
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2024-04-12 12:57:02 +0200

    Removed type parameter

--- VKUtils.c
+++ VKUtils.c
@@ -1,18 +1,37 @@
+#include <memory.h>
 #include "VKUtils.h"
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
 
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "MemoryLeak"
-void* VKUtils_vec_alloc(size_t size_el, size_t capacity,
-                        ptrdiff_t off_size,
-                        ptrdiff_t off_capacity,
-                        ptrdiff_t off_data)
-{
-    char* vec_mem = malloc(size_el * capacity + off_data);
+void* VKU_vec_alloc(size_t elem_size, size_t capacity) {
+    size_t data_offset = offsetof(VKU_vec_t, data);
+    char* vec_mem = malloc(elem_size * capacity + data_offset);
     if (vec_mem == NULL) {
         return NULL;
     }
-    *((size_t*)((char*)vec_mem + off_size)) = 0;
-    *((size_t*)((char*)vec_mem + off_capacity)) = capacity;
-    return vec_mem + off_data;
+    *((size_t*)((char*)vec_mem + offsetof(VKU_vec_t, elem_size))) = elem_size;
+    *((size_t*)((char*)vec_mem + offsetof(VKU_vec_t, size))) = 0;
+    *((size_t*)((char*)vec_mem + offsetof(VKU_vec_t, capacity))) = capacity;
+    return vec_mem + data_offset;
 }
+
+void* VKU_vec_realloc(VKU_vec_t* vec, size_t new_capacity) {
+    if (vec->capacity == new_capacity) {
+        return vec->data;
+    }
+    VKU_vec_t* new_vec = (VKU_vec_t*)(VKU_vec_alloc(vec->elem_size, new_capacity) - offsetof(VKU_vec_t, data));
+    new_vec->capacity = new_capacity;
+    new_vec->size = MIN(vec->size, new_capacity);
+    new_vec->elem_size = vec->elem_size;
+    memcpy(new_vec->data, vec->data, new_vec->size*new_vec->elem_size);
+    free(vec);
+    return new_vec->data;
+}
+
+void* VKU_vec_shrink_to_fit(VKU_vec_t* vec) {
+    return VKU_vec_realloc(vec, vec->size);
+}
+
 #pragma clang diagnostic pop
--- VKUtils.h
+++ VKUtils.h
@@ -5,25 +5,28 @@
 #include <malloc.h>
 #include <assert.h>
 
-void* VKUtils_vec_alloc(size_t size_el, size_t capacity,
-                        ptrdiff_t off_size,
-                        ptrdiff_t off_capacity,
-                        ptrdiff_t off_data);
+typedef struct {
+    size_t elem_size;
+    size_t size;
+    size_t capacity;
+    void* data;
+} VKU_vec_t;
 
-#define VEC_DEF_TYPE(T) typedef struct { size_t size; size_t capacity; T* data;} vec_ ## T
+void* VKU_vec_alloc(size_t elem_size, size_t capacity);
+void* VKU_vec_realloc(VKU_vec_t* vec, size_t new_capacity);
 
-#define VEC_ALLOC(T, SIZE) (T*)VKUtils_vec_alloc(sizeof(T), SIZE,              \
-                                                 offsetof(vec_ ## T, size),    \
-                                                 offsetof(vec_ ## T, capacity),\
-                                                 offsetof(vec_ ## T, data))
 
-#define VEC_SIZE(P, T) ((vec_ ## T*)((char*)P - offsetof(vec_ ## T, data)))->size
-#define VEC_CAPACITY(P, T) ((vec_ ## T*)((char*)P - offsetof(vec_ ## T, data)))->capacity
-#define VEC_FREE(P, T)  free(((vec_ ## T*)((char*)P - offsetof(vec_ ## T, data))))
-#define VEC_ADD(P, D, T) do {                    \
-    assert(VEC_SIZE(P, T) < VEC_CAPACITY(P, T)); \
-    *(P + VEC_SIZE(P, T)) = (D);                 \
-    VEC_SIZE(P, T)++;                            \
+#define VEC_ALLOC(T, SIZE) (T*)VKU_vec_alloc(sizeof(T), SIZE)
+#define VEC_T(P) (VKU_vec_t*)((char*)P - offsetof(VKU_vec_t, data))
+#define VEC_SIZE(P) (VEC_T(P))->size
+#define VEC_CAPACITY(P) (VEC_T(P))->capacity
+#define VEC_FREE(P)  free(VEC_T(P))
+#define VEC_SHRINK_TO_FIT(P) VKU_vec_realloc(VEC_T(P), VEC_SIZE(P))
+
+#define VEC_ADD(P, D) do {                       \
+    assert(VEC_SIZE(P) < VEC_CAPACITY(P));       \
+    *(P + VEC_SIZE(P)) = (D);                    \
+    VEC_SIZE(P)++;                               \
 } while(0)
 
 #endif // VKUTILS_H

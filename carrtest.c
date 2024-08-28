286ed78c34f4874872718867a9ccd7da95dcf3f1
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2024-08-28 15:40:03 +0200

    Lazy array initialization, ring buffer and OOM strategy.

--- carrtest.c
+++ carrtest.c
@@ -15,10 +15,10 @@ void test_array_int() {
 
     if (ARRAY_CAPACITY(a) != 10) exit(1);
 
-    ARRAY_PUSH_BACK(&a, 0);
-    ARRAY_PUSH_BACK(&a, 1);
-    ARRAY_PUSH_BACK(&a, 2);
-    ARRAY_PUSH_BACK(&a, 3);
+    ARRAY_PUSH_BACK(a, 0);
+    ARRAY_PUSH_BACK(a, 1);
+    ARRAY_PUSH_BACK(a, 2);
+    ARRAY_PUSH_BACK(a, 3);
 
     if (ARRAY_SIZE(a) != 4) exit(1);
 
@@ -35,10 +35,10 @@ void test_array_pchar() {
 
     if (ARRAY_CAPACITY(a) != 10) exit(1);
 
-    ARRAY_PUSH_BACK(&a, "0");
-    ARRAY_PUSH_BACK(&a, "1");
-    ARRAY_PUSH_BACK(&a, "2");
-    ARRAY_PUSH_BACK(&a, "3");
+    ARRAY_PUSH_BACK(a, "0");
+    ARRAY_PUSH_BACK(a, "1");
+    ARRAY_PUSH_BACK(a, "2");
+    ARRAY_PUSH_BACK(a, "3");
 
     if (ARRAY_SIZE(a) != 4) exit(1);
 
@@ -57,15 +57,15 @@ void test_array_shrink_to_fit() {
 
     if (ARRAY_CAPACITY(a) != 10) exit(1);
 
-    ARRAY_PUSH_BACK(&a, "0");
-    ARRAY_PUSH_BACK(&a, "1");
-    ARRAY_PUSH_BACK(&a, "2");
-    ARRAY_PUSH_BACK(&a, "3");
+    ARRAY_PUSH_BACK(a, "0");
+    ARRAY_PUSH_BACK(a, "1");
+    ARRAY_PUSH_BACK(a, "2");
+    ARRAY_PUSH_BACK(a, "3");
 
 
     if (ARRAY_SIZE(a) != 4) exit(1);
 
-    ARRAY_SHRINK_TO_FIT(&a);
+    ARRAY_SHRINK_TO_FIT(a);
 
     if (a == aa) exit(1);
 
@@ -84,10 +84,10 @@ void test_array_shrink_to_fit() {
 void test_array_expand() {
     pchar* a = ARRAY_ALLOC(pchar, 3);
 
-    ARRAY_PUSH_BACK(&a, "0");
-    ARRAY_PUSH_BACK(&a, "1");
-    ARRAY_PUSH_BACK(&a, "2");
-    ARRAY_PUSH_BACK(&a, "3");
+    ARRAY_PUSH_BACK(a, "0");
+    ARRAY_PUSH_BACK(a, "1");
+    ARRAY_PUSH_BACK(a, "2");
+    ARRAY_PUSH_BACK(a, "3");
 
     if (ARRAY_SIZE(a) != 4) exit(1);
     if (ARRAY_CAPACITY(a) <= 3) exit(1);
@@ -101,10 +101,126 @@ void test_array_expand() {
     ARRAY_FREE(a);
 }
 
+#define TEST_USER_DATA1 123
+#define TEST_USER_DATA2 321
+static void pchar_apply_callback(pchar* c) {
+    if (c == NULL) exit(1);
+}
+static void pchar_apply_leading_callback(pchar* c, int user_data1, int user_data2) {
+    if (c == NULL) exit(1);
+    if (user_data1 != TEST_USER_DATA1) exit(1);
+    if (user_data2 != TEST_USER_DATA2) exit(1);
+}
+static void pchar_apply_trailing_callback(int user_data1, int user_data2, pchar* c) {
+    if (c == NULL) exit(1);
+    if (user_data1 != TEST_USER_DATA1) exit(1);
+    if (user_data2 != TEST_USER_DATA2) exit(1);
+}
+
+void test_array_null_safe() {
+    pchar* a = NULL;
+
+    if (ARRAY_SIZE(a) != 0) exit(1);
+    if (ARRAY_CAPACITY(a) != 0) exit(1);
+    ARRAY_FREE(a);
+
+    ARRAY_APPLY(a, pchar_apply_callback);
+    ARRAY_APPLY_LEADING(a, pchar_apply_leading_callback, TEST_USER_DATA1, TEST_USER_DATA2);
+    ARRAY_APPLY_TRAILING(a, pchar_apply_trailing_callback, TEST_USER_DATA1, TEST_USER_DATA2);
+
+    ARRAY_PUSH_BACK(a, "test");
+    if (ARRAY_SIZE(a) != 1) exit(1);
+    if (ARRAY_CAPACITY(a) < 1) exit(1);
+    if (strcmp(a[0], "test") != 0) exit(1);
+
+    ARRAY_FREE(a);
+}
+
+void test_array_ensure_capacity() {
+    pchar* a = NULL;
+
+    ARRAY_ENSURE_CAPACITY(a, 1);
+    if (ARRAY_CAPACITY(a) < 1) exit(1);
+
+    size_t expanded_capacity = ARRAY_CAPACITY(a) + 1;
+    ARRAY_ENSURE_CAPACITY(a, expanded_capacity);
+    if (ARRAY_CAPACITY(a) < expanded_capacity) exit(1);
+
+    ARRAY_FREE(a);
+}
+
+void test_array_resize() {
+    pchar* a = NULL;
+
+    ARRAY_RESIZE(a, 10);
+    if (ARRAY_SIZE(a) != 10) exit(1);
+    if (ARRAY_CAPACITY(a) < 10) exit(1);
+
+    ARRAY_RESIZE(a, 20);
+    if (ARRAY_SIZE(a) != 20) exit(1);
+    if (ARRAY_CAPACITY(a) < 20) exit(1);
+
+    ARRAY_FREE(a);
+}
+
+void test_ring_buffer_null_safe() {
+    pchar* b = NULL;
+
+    if (RING_BUFFER_SIZE(b) != 0) exit(1);
+    if (RING_BUFFER_CAPACITY(b) != 0) exit(1);
+    RING_BUFFER_FREE(b);
+
+    RING_BUFFER_PUSH(b, "test");
+    if (RING_BUFFER_SIZE(b) != 1) exit(1);
+    if (RING_BUFFER_CAPACITY(b) < 1) exit(1);
+
+    RING_BUFFER_FREE(b);
+}
+
+void test_ring_buffer_wrap() {
+    const int EXPAND_COUNT = 1000;
+    const int INNER_COUNT  = 1000;
+    int* b = NULL;
+
+    int read = 0;
+    int write = 0;
+    for (int i = 0; i < EXPAND_COUNT; i++) {
+        for (int j = 0; j < INNER_COUNT; j++) {
+            RING_BUFFER_PUSH(b, write);
+            write++;
+            int* value = RING_BUFFER_PEEK(b);
+            if (value == NULL) exit(1);
+            if (*value != read) exit(1);
+            read++;
+            RING_BUFFER_POP(b);
+        }
+        RING_BUFFER_PUSH(b, write);
+        write++;
+    }
+    if (RING_BUFFER_SIZE(b) != EXPAND_COUNT) exit(1);
+
+    for (int i = 0; i < EXPAND_COUNT; i++) {
+        int* value = RING_BUFFER_PEEK(b);
+        if (value == NULL) exit(1);
+        if (*value != read) exit(1);
+        read++;
+        RING_BUFFER_POP(b);
+    }
+    if (RING_BUFFER_PEEK(b) != NULL) exit(1);
+    if (RING_BUFFER_SIZE(b) != 0) exit(1);
+
+    RING_BUFFER_FREE(b);
+}
+
 int main(int argc, char *argv[] ) {
     RUN_TEST(array_int);
     RUN_TEST(array_pchar);
     RUN_TEST(array_shrink_to_fit);
     RUN_TEST(array_expand);
+    RUN_TEST(array_null_safe);
+    RUN_TEST(array_ensure_capacity);
+    RUN_TEST(array_resize);
+    RUN_TEST(ring_buffer_null_safe);
+    RUN_TEST(ring_buffer_wrap);
     return 0;
 }
